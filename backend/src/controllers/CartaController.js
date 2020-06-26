bfff5c69683cf4d932998fdf3db0f8d2e4875d5d
Fabio Siqueira <fabiohfs85@gmail.com>
2020-06-26 17:41:30 -0300

    Chart method developed and removed mobile project

--- backend/src/controllers/CartaController.js
+++ backend/src/controllers/CartaController.js
@@ -1,39 +1,27 @@
-const axios = require('axios')
-const xml2json = require('xml2json')
-const path = require('path')
-const fs = require('fs')
+const axios = require('axios');
+const xml2json = require('xml2json');
+const path = require('path');
+const fs = require('fs');
 
 module.exports = {
     async getChart(req, res) {
-        var { icao } = req.query
-        var { tipo } = req.query
+        var { icao } = req.query;
+        var { tipo } = req.query;
 
-        var url = process.env.BASE_URL
-        url = url.replace('=Key', '=' + process.env.API_KEY)
-        url = url.replace('=Pass', '=' + process.env.API_PWD)
+        var url = process.env.BASE_URL;
+        url = url.replace('=Key', '=' + process.env.API_KEY);
+        url = url.replace('=Pass', '=' + process.env.API_PWD);
 
-        url = url.concat('&area=cartas&IcaoCode=', icao, '&tipo=', tipo)
+        url = url.concat('&area=cartas&IcaoCode=', icao, '&tipo=', tipo);
 
         await axios.get(url, {
         }).then(async function (data) {
-            var result = data.data
+            var result = data.data;
 
-            var responseJson = xml2json.toJson(result)
-            var downloadLink = JSON.parse(responseJson).aisweb.cartas.item.link
+            var responseJson = xml2json.toJson(result);
+            var downloadLink = JSON.parse(responseJson).aisweb.cartas.item.link;
 
-            //TODO: Verificar como será tratado para o front
-            const filePath = path.resolve(__dirname, '/Users/fabio/Documents/', tipo + ' - ' + icao + '.pdf')
-            const writer = fs.createWriteStream(filePath)
-
-            const response = await axios({
-                url: downloadLink,
-                method: 'GET',
-                responseType: 'stream'
-            })
-
-            response.data.pipe(writer)
-
-            return res.send({ "message": "file downloaded" })
+            return res.send({ "fileLink": downloadLink });
         })
 
         return res.end()
